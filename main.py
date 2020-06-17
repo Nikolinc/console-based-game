4f84bf6809c6d46760686c13edbfdb9ac4bdd053
pyc0d3 <pyc0d3@gmail.com>
2020-06-17 21:26:50 +0300

    saves folder now creates in Game class

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from src import game
-import os
 
 if __name__ == '__main__':
-	os.makedirs('saves', exist_ok=True)
-	save_file_location = os.path.join(
-		os.path.dirname(__file__), 'saves', 'save.pyc0d3')
-	game.Game(save_file_location).run()
+	game.Game().run()
--- src/game.py
+++ src/game.py
@@ -2,21 +2,27 @@ from src.scene.ui import Menu
 from src.scene import BaseScene
 from src.util import check_isinstance
 import pickle
+import os
 
 
 class Game:
-	def __init__(self, save_file_location):
+	def __init__(self):
+		self.PATH_TO_SAVES = os.path.join(os.path.dirname(__file__), '..', 'saves')
+		os.makedirs(self.PATH_TO_SAVES, exist_ok=True)
+		self.SAVE_FILE_NAME = 'save.pyc0d3'
+		self.PATH_TO_SAVE_FILE = os.path.join(
+			self.PATH_TO_SAVES, self.SAVE_FILE_NAME)
+
 		self.is_running = True
-		self.save_file_location = save_file_location
 		self._scene = Menu()
 		self._prev_scene = Menu()
 
 	def save(self):
-		with open(self.save_file_location, 'wb') as file:
+		with open(self.PATH_TO_SAVE_FILE, 'wb') as file:
 			pickle.dump(self.__dict__, file)
 
 	def load(self):
-		with open(self.save_file_location, 'rb') as file:
+		with open(self.PATH_TO_SAVE_FILE, 'rb') as file:
 			self.__dict__.update(pickle.load(file))
 
 	def run(self):
