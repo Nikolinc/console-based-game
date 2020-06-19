1b2b24b67c0a0cfcdc0ca54e5f8882812da1927b
Nikolinc <54713704+Nikolinc@users.noreply.github.com>
2020-06-20 01:23:42 +0500

    Add files via upload

--- src/item/weapon/melee.py
+++ src/item/weapon/melee.py
@@ -6,3 +6,22 @@ class Sword(BaseWeapon):
 		stats = self.Stats(owner, damage=5)
 		info = self.Info(owner, name='Sword')
 		super().__init__(owner, stats, info)
+
+
+class Whinger(BaseWeapon):
+	def __init__(self, owner):
+		stats = self.Stats(owner, damage=3)
+		info = self.Info(owner, name='Whinger')
+		super().__init__(owner, stats, info)
+
+class Hammer(BaseWeapon):
+	def __init__(self, owner):
+		stats = self.Stats(owner, damage=7)
+		info = self.Info(owner, name='Whinger')
+		super().__init__(owner, stats, info)
+
+class Brand (BaseWeapon):
+	def __init__(self, owner):
+		stats = self.Stats(owner, damage=4)
+		info = self.Info(owner, name='Whinger')
+		super().__init__(owner, stats, info)
--- src/item/weapon/ranged.py
+++ src/item/weapon/ranged.py
@@ -6,3 +6,15 @@ class Staff(BaseWeapon):
 		stats = self.Stats(owner, damage=7)
 		info = self.Info(owner, name='Staff')
 		super().__init__(owner, stats, info)
+
+class Pikestaff(BaseWeapon):
+	def __init__(self, owner):
+		stats = self.Stats(owner, damage=6)
+		info = self.Info(owner, name='Staff')
+		super().__init__(owner, stats, info)
+
+class Bow (BaseWeapon):
+	def __init__(self, owner):
+		stats = self.Stats(owner, damage=8)
+		info = self.Info(owner, name='Staff')
+		super().__init__(owner, stats, info)
