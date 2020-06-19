269a2fa0a3af6df33d8ff0b65a96fd6e0160d00f
Nikolinc <54713704+Nikolinc@users.noreply.github.com>
2020-06-20 01:23:07 +0500

    Add files via upload

--- src/item/armor/heavy.py
+++ src/item/armor/heavy.py
@@ -4,3 +4,7 @@ from . import BaseArmor
 class ChestPlate(BaseArmor):
 	def __init__(self, owner):
 		pass
+
+class Breastplate(BaseArmor):
+	def __init__(self, owner):
+		pass
--- src/item/armor/light.py
+++ src/item/armor/light.py
@@ -4,3 +4,16 @@ from . import BaseArmor
 class Cloak(BaseArmor):
 	def __init__(self, owner):
 		pass
+
+
+class Robe(BaseArmor):
+	def __init__(self, owner):
+		pass
+
+class Jacket (BaseArmor):
+	def __init__(self, owner):
+		pass
+
+class Pallium (BaseArmor):
+	def __init__(self, owner):
+		pass
